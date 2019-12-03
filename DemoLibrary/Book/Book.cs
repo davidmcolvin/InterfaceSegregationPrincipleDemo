d6fe7220ad5d9518b2638cfb3ea23e79ec9143b6
david colvin <davidmcolvin@gmail.com>
2019-12-02 21:38:38 -0500

    Implement the Interface Segregation Principle

--- DemoLibrary/Book/Book.cs
+++ DemoLibrary/Book/Book.cs
@@ -6,17 +6,15 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-  public class DVD : ILibraryItem
+  public class Book : IBorrowableBook
   {
     public string LibraryId { get; set; }
     public string Title { get; set; }
-    public string Author { get; set; } = "";
-    public int Pages { get; set; } = -1;
+    public string Author { get; set; }
+    public int Pages { get; set; }
     public int CheckOutDurationInDays { get; set; } = 14;
     public string Borrower { get; set; }
     public DateTime BorrowDate { get; set; }
-    public List<string> Actors { get; set; }
-    public int RuntimeInMinutes { get; set; }
 
     public void CheckOut(string borrower)
     {
--- DemoLibrary/DVD/DVD.cs
+++ DemoLibrary/DVD/DVD.cs
@@ -6,29 +6,30 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-  public class ReferenceBook : ILibraryItem
+  public class DVD : IBorrowableDVD
   {
     public string LibraryId { get; set; }
     public string Title { get; set; }
-    public string Author { get; set; }
-    public int Pages { get; set; }
-    public int CheckOutDurationInDays { get; set; } = 0;
+    public int CheckOutDurationInDays { get; set; } = 14;
     public string Borrower { get; set; }
     public DateTime BorrowDate { get; set; }
+    public List<string> Actors { get; set; }
+    public int RuntimeInMinutes { get; set; }
 
     public void CheckOut(string borrower)
     {
-      throw new NotImplementedException();
+      Borrower = borrower;
+      BorrowDate = DateTime.Now;
     }
 
     public void CheckIn()
     {
-      throw new NotImplementedException();
+      Borrower = "";
     }
 
     public DateTime GetDueDate()
     {
-      throw new NotImplementedException();
+      return BorrowDate.AddDays(CheckOutDurationInDays);
     }
   }
 }
